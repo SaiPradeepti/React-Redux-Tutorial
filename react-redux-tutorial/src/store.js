0b1bfe2707d97c54a89eb0fb3e9b59d55581c308
Sai Pradeepti <saipradeepti14@gmail.com>
2022-04-01 15:25:34 +0530

    first commit - setting up redux toolkit

---
+++ react-redux-tutorial/src/store.js
@@ -0,0 +1,9 @@
+// import configureStore
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './features/cart/cartSlice'
+
+export const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    }
+})
